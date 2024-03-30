540bb99b3af1407bdbb35e5d6e63d6b06cc0b663
Duc Cuong <nduccuong65@gmail.com>
2024-03-30 21:03:45 +0700

    tt
    ege

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -10,7 +11,14 @@ const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
 
 // CREATE A NEW USER AND SAVE TO DB 
-
+const newUser = await prisma.user.create({
+    data : {
+        username,
+        email,
+        password : hashedPassword,
+    },
+});
+console.log(newUser)
 
 };
 
